62d89610e33fdaf7d354340ff8edef96ad04c1ba
Isagani Lapira <lapiraisagani@gmail.com>
2024-04-12 17:19:54 +0800

    Optimization of service retrieval

--- eyeoptic_app/lib/services/firestore.dart
+++ eyeoptic_app/lib/services/firestore.dart
@@ -1,18 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:eyeoptic_app/model/servicemodel.dart';
 
 class FireStoreService {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
-  Future<List<ServiceModel>> fetchService() async {
-    var snapShot = await _firestore.collection('services').get();
-    List<ServiceModel> services = snapShot.docs
-        .map((service) => ServiceModel(
-              name: service['name'],
-              description: service['description'],
-              date: service['date_created'],
-            ))
-        .toList();
-    return services;
-  }
+  Stream<QuerySnapshot> serviceStream() =>
+      _firestore.collection('services').snapshots();
+
 }
