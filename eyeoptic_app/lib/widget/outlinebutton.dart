10d010bf0a6354e731f186ee378effb7243a599a
Isagani Lapira <lapiraisagani@gmail.com>
2024-04-15 17:10:40 +0800

    add: dynamic icon for icontext widget

---
+++ eyeoptic_app/lib/widget/outlinebutton.dart
@@ -0,0 +1,34 @@
+import 'package:eyeoptic_app/theme/colors.dart';
+import 'package:eyeoptic_app/widget/iconwithtext.dart';
+import 'package:flutter/material.dart';
+
+class OutlinedIconTextButton extends StatelessWidget {
+  final Function onTap;
+  final String text;
+  const OutlinedIconTextButton({
+    super.key,
+    required this.onTap,
+    required this.text,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: onTap(),
+      style: Theme.of(context).textButtonTheme.style!.copyWith(
+            backgroundColor: const MaterialStatePropertyAll(Colors.white),
+            side: const MaterialStatePropertyAll(
+              BorderSide(
+                color: AppColor.primaryColor,
+                width: 1.5,
+              ),
+            ),
+          ),
+      child: IconText(
+        color: AppColor.primaryColor,
+        title: text,
+        icon: Icons.add,
+      ),
+    );
+  }
+}
