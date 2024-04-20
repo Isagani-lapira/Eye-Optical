7c8d2403b6df809026970ffef1c26361848cac4b
Isagani Lapira <lapiraisagani@gmail.com>
2024-04-20 13:08:38 +0800

    Finish edit service UI and functionality

--- eyeoptic_app/lib/screen/admin/tabs/service content/editservicetab.dart
+++ eyeoptic_app/lib/screen/admin/tabs/service content/editservicetab.dart
@@ -1,7 +1,11 @@
 import 'package:eyeoptic_app/model/servicemodel.dart';
+import 'package:eyeoptic_app/provider/servicetabprovider.dart';
 import 'package:eyeoptic_app/services/firestore.dart';
+import 'package:eyeoptic_app/theme/colors.dart';
+import 'package:eyeoptic_app/widget/iconwithtext.dart';
 import 'package:eyeoptic_app/widget/textfield.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class EditServiceTab extends StatelessWidget {
   final ServiceModel serviceModel;
@@ -12,12 +16,24 @@ class EditServiceTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var provider = Provider.of<ServiceTabProvider>(context, listen: false);
     String title = '';
     String description = '';
-    return Center(
+    return SingleChildScrollView(
       child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
         children: [
+          Align(
+            alignment: Alignment.topLeft,
+            child: GestureDetector(
+              onTap: () => provider.toggleDisplay(ServiceSection.mainservice),
+              child: const IconText(
+                title: 'Edit service',
+                color: AppColor.textColor,
+                icon: Icons.arrow_back,
+                isHeadlinerText: true,
+              ),
+            ),
+          ),
           CustomField(
             labelTxt: 'Title',
             hint: serviceModel.name,
@@ -29,20 +45,35 @@ class EditServiceTab extends StatelessWidget {
             hint: serviceModel.description,
             onChange: (value) => description = value,
           ),
-          TextButton(
-              onPressed: () {
-                if (title.isNotEmpty && description.isNotEmpty) {
-                  FireStoreService fireStoreService = FireStoreService();
+          SizedBox(
+            width: MediaQuery.of(context).size.width * 0.4,
+            child: TextButton(
+                onPressed: () {
+                  if (title.isNotEmpty && description.isNotEmpty) {
+                    FireStoreService fireStoreService = FireStoreService();
 
-                  fireStoreService.updateService(ServiceModel(
-                      id: serviceModel.id,
-                      name: title,
-                      description: description,
-                      iconName: serviceModel.iconName,
-                      date: serviceModel.date));
-                }
-              },
-              child: Text('Save changes')),
+                    fireStoreService.updateService(ServiceModel(
+                        id: serviceModel.id,
+                        name: title,
+                        description: description,
+                        iconName: serviceModel.iconName,
+                        date: serviceModel.date));
+                  }
+                },
+                child: Text('Save changes')),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 8.0),
+            width: MediaQuery.of(context).size.width * 0.4,
+            child: TextButton(
+              style: Theme.of(context).textButtonTheme.style!.copyWith(
+                  backgroundColor:
+                      const MaterialStatePropertyAll(AppColor.cancelButton)),
+              onPressed: () =>
+                  provider.toggleDisplay(ServiceSection.mainservice),
+              child: const Text('Cancel'),
+            ),
+          ),
         ],
       ),
     );
