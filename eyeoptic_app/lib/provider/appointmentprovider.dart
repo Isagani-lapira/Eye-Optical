e9f595b315bb7af4eacacc0cd9ac094704cc9af1
Isagani Lapira <lapiraisagani@gmail.com>
2024-05-04 11:58:12 +0800

    use provider for displaying appointment tab with retrieval of doctor name and service name

---
+++ eyeoptic_app/lib/provider/appointmentprovider.dart
@@ -0,0 +1,40 @@
+import 'package:eyeoptic_app/model/appointmentmodel.dart';
+import 'package:eyeoptic_app/services/doctorstore.dart';
+import 'package:eyeoptic_app/services/firestore.dart';
+import 'package:flutter/material.dart';
+
+class AppointmentProvider extends ChangeNotifier {
+  final FireStoreService _service = FireStoreService();
+  final FireStoreDoctor _doctor = FireStoreDoctor();
+  List<AppointmentModel> _appointmentModel = [];
+
+  void setAppointmentModel(List<AppointmentModel> model) =>
+      _appointmentModel = model;
+
+  List<AppointmentModel> get model => _appointmentModel;
+
+  String getDate(int index) => _appointmentModel[index].date;
+  String getServiceID(int index) => _appointmentModel[index].serviceID;
+
+  //slice the time into two
+  Map<String, String> getTime(int index) {
+    String timeStr = _appointmentModel[index].time;
+    int separator = timeStr.indexOf(':') + 3;
+    String time = timeStr.substring(0, separator);
+    String meridiem = timeStr.substring(separator);
+
+    return {'time': time, 'meridiem': meridiem};
+  }
+
+  Future<String?> getServiceName(String serviceID) async {
+    try {
+      return await _service.getServiceName(serviceID);
+    } catch (e) {
+      throw Exception(e);
+    }
+  }
+
+  Future<String?> getDoctorName(String doctorId) async {
+    return await _doctor.getDoctorName(doctorId);
+  }
+}
