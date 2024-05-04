6ac931b031004f12359097c89152cacc5234576c
Isagani Lapira <lapiraisagani@gmail.com>
2024-05-04 11:58:45 +0800

    Placed alltimeslot to constant file

--- eyeoptic_app/lib/utils/const.dart
+++ eyeoptic_app/lib/utils/const.dart
@@ -23,3 +23,14 @@ const String kcharacters = 'abcdefghijklmnopqrstuvwxyz0123456789';
 const CircularProgressIndicator kLoader = CircularProgressIndicator(
   color: AppColor.primaryColor,
 );
+
+List<String> kAllTimeSlots = [
+  '9:00am',
+  '10:00am',
+  '11:00am',
+  '1:00pm',
+  '2:00pm',
+  '3:00pm',
+  '4:00pm',
+  '5:00pm'
+];
