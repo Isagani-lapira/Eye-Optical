94635c23eac2bd224d75fa1cd8b1951f2e00ecba
Isagani Lapira <lapiraisagani@gmail.com>
2024-05-04 12:00:21 +0800

    display dynamic data for timeline card with reusable future text

--- eyeoptic_app/lib/widget/timeline/timeline_card.dart
+++ eyeoptic_app/lib/widget/timeline/timeline_card.dart
@@ -1,22 +1,29 @@
-import 'package:eyeoptic_app/model/appointmentmodel.dart';
+import 'package:eyeoptic_app/provider/appointmentprovider.dart';
 import 'package:eyeoptic_app/theme/colors.dart';
+import 'package:eyeoptic_app/utils/string.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class TimelineCard extends StatelessWidget {
   final bool isHighlighted;
   final EdgeInsets marginBottom;
-  final AppointmentModel model;
+  final int index;
   const TimelineCard({
     super.key,
-    required this.model,
+    required this.index,
     this.isHighlighted = false,
     this.marginBottom = const EdgeInsets.only(bottom: 8.0),
   });
 
-  // TODO: make the data dynamic and use provider
   @override
   Widget build(BuildContext context) {
     Color primaryColor = (isHighlighted) ? Colors.white : AppColor.textColor;
+    var provider = Provider.of<AppointmentProvider>(context);
+    var model = provider.model[index];
+
+    Map time = provider.getTime(index);
+    Future<String?> serviceName = provider.getServiceName(model.serviceID);
+    Future<String?> doctorName = provider.getDoctorName(model.assignedDoctor!);
     return Container(
       margin: marginBottom,
       child: Row(
@@ -24,15 +31,13 @@ class TimelineCard extends StatelessWidget {
         children: [
           Column(
             children: [
-              Text(
-                '11:35',
-                style: Theme.of(context)
-                    .textTheme
-                    .bodyMedium!
-                    .copyWith(fontWeight: FontWeight.w600),
-              ),
+              Text(time['time'],
+                  style: Theme.of(context)
+                      .textTheme
+                      .bodyMedium!
+                      .copyWith(fontWeight: FontWeight.w600)),
               const SizedBox(height: 8.0),
-              Text('PM',
+              Text(time['meridiem'],
                   style: Theme.of(context).textTheme.bodyMedium!.copyWith(
                       color: AppColor.hintColor, fontWeight: FontWeight.w500)),
             ],
@@ -57,18 +62,20 @@ class TimelineCard extends StatelessWidget {
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Text('Contact Lens',
-                          style: TextStyle(color: primaryColor)),
+                      FutureText(
+                        futureTxt: serviceName,
+                        style: TextStyle(color: primaryColor),
+                      ),
                       Icon(Icons.more_vert, color: primaryColor, size: 24.0),
                     ],
                   ),
                   const SizedBox(height: 10.0),
-                  iconCard(context, Icons.date_range_outlined, model.date,
-                      primaryColor),
-                  iconCard(context, Icons.room_outlined, 'Eye Care Center',
-                      primaryColor),
-                  iconCard(context, Icons.health_and_safety,
-                      'Brooklyn Williamson', primaryColor),
+                  iconCard(context, Icons.date_range_outlined, primaryColor,
+                      label: provider.getDate(index)),
+                  iconCard(context, Icons.room_outlined, primaryColor,
+                      label: AppString.appName),
+                  iconCard(context, Icons.health_and_safety, primaryColor,
+                      futureText: doctorName),
                 ],
               ),
             ),
@@ -78,21 +85,47 @@ class TimelineCard extends StatelessWidget {
     );
   }
 
-  Widget iconCard(
-      BuildContext context, IconData icondata, String label, Color color) {
+  Widget iconCard(BuildContext context, IconData icondata, Color color,
+      {Future<String?>? futureText, String? label}) {
+    TextStyle defaultStyle =
+        Theme.of(context).textTheme.bodySmall!.copyWith(color: color);
     return Container(
       margin: const EdgeInsets.only(bottom: 5.0),
       child: Wrap(
         children: [
           Icon(icondata, color: color, size: 20.0),
           const SizedBox(width: 8.0),
-          Text(label,
-              style: Theme.of(context)
-                  .textTheme
-                  .bodySmall!
-                  .copyWith(color: color)),
+          if (futureText == null) ...[
+            Text(label!, style: defaultStyle)
+          ] else ...[
+            FutureText(futureTxt: futureText, style: defaultStyle)
+          ],
         ],
       ),
+    );
+  }
+}
+
+class FutureText extends StatelessWidget {
+  final Future<String?> futureTxt;
+  final TextStyle? style;
+  const FutureText({super.key, required this.futureTxt, this.style});
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: futureTxt,
+      builder: ((context, snapshot) {
+        String text;
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          text = 'Loading';
+        } else if (snapshot.hasError) {
+          text = 'Error';
+        } else {
+          text = snapshot.data ?? 'Not available';
+        }
+        return Text(text, style: style);
+      }),
     );
   }
 }
