e78e215c27473b54167f9be90457d08aabbfd339
Isagani Lapira <lapiraisagani@gmail.com>
2024-05-05 15:46:26 +0800

    Switch process from provider to model

--- eyeoptic_app/lib/model/appointmentmodel.dart
+++ eyeoptic_app/lib/model/appointmentmodel.dart
@@ -1,3 +1,7 @@
+import 'package:eyeoptic_app/services/doctorstore.dart';
+import 'package:eyeoptic_app/services/firestore.dart';
+import 'package:intl/intl.dart';
+
 class AppointmentModel {
   final String? id;
   final String uID;
@@ -15,11 +19,70 @@ class AppointmentModel {
     this.id,
   });
 
+  static final FireStoreService _service = FireStoreService();
+  static final FireStoreDoctor _doctor = FireStoreDoctor();
+
   static String formattedDate(String date) {
     int spaceIndex = date.indexOf(' ');
     if (spaceIndex >= 0) {
       return date.substring(0, spaceIndex);
     }
     return date;
+  }
+
+  static Future<String?> getServiceName(String serviceID) async {
+    try {
+      return await _service.getServiceName(serviceID);
+    } catch (e) {
+      throw Exception(e);
+    }
+  }
+
+  static Future<String?> getDoctorName(String doctorId) async {
+    String? doctorName = await _doctor.getDoctorName(doctorId);
+    return 'Dr. $doctorName';
+  }
+
+  static List<AppointmentModel> getAppointmentData(var data) {
+    List<AppointmentModel> appointmentModel = [];
+    DateTime currentTime = DateTime.now();
+
+    for (var appointment in data) {
+      String date = formattedStringDate(appointment['date']);
+      int appointmentHour = get24HourFormat(appointment['time']);
+
+      // Check if the schedule is already done
+      if (appointmentHour >= currentTime.hour) {
+        appointmentModel.add(
+          AppointmentModel(
+            id: appointment.id,
+            uID: appointment['uid'],
+            serviceID: appointment['serviceid'],
+            date: date,
+            time: appointment['time'],
+            assignedDoctor: appointment['assigned_doctor'],
+          ),
+        );
+      }
+    }
+    return appointmentModel;
+  }
+
+  static int get24HourFormat(String time) {
+    int hour = int.parse(time.split(':')[0]);
+    String amPm = time.split('00')[1].toLowerCase();
+    if (amPm == 'pm' && hour != 12) {
+      hour += 12;
+    } else if (amPm == 'am' && hour == 12) {
+      hour = 0;
+    }
+
+    return hour;
+  }
+
+  static String formattedStringDate(String date) {
+    DateTime inputDate = DateTime.parse(date);
+    String formattedDate = DateFormat('MMM dd, yyyy').format(inputDate);
+    return formattedDate;
   }
 }
--- eyeoptic_app/lib/provider/appointmentprovider.dart
+++ eyeoptic_app/lib/provider/appointmentprovider.dart
@@ -1,11 +1,7 @@
 import 'package:eyeoptic_app/model/appointmentmodel.dart';
-import 'package:eyeoptic_app/services/doctorstore.dart';
-import 'package:eyeoptic_app/services/firestore.dart';
 import 'package:flutter/material.dart';
 
 class AppointmentProvider extends ChangeNotifier {
-  final FireStoreService _service = FireStoreService();
-  final FireStoreDoctor _doctor = FireStoreDoctor();
   List<AppointmentModel> _appointmentModel = [];
 
   void setAppointmentModel(List<AppointmentModel> model) =>
@@ -29,17 +25,5 @@ class AppointmentProvider extends ChangeNotifier {
     String meridiem = timeStr.substring(separator);
 
     return {'time': time, 'meridiem': meridiem};
-  }
-
-  Future<String?> getServiceName(String serviceID) async {
-    try {
-      return await _service.getServiceName(serviceID);
-    } catch (e) {
-      throw Exception(e);
-    }
-  }
-
-  Future<String?> getDoctorName(String doctorId) async {
-    return await _doctor.getDoctorName(doctorId);
   }
 }
