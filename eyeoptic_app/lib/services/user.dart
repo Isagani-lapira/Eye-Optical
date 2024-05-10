6149be8bd57aebf72c6c307786d94477d085d202
Isagani Lapira <lapiraisagani@gmail.com>
2024-05-10 10:31:10 +0800

    sign up process with handling error

---
+++ eyeoptic_app/lib/services/user.dart
@@ -0,0 +1,33 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:eyeoptic_app/model/usermodel.dart';
+import 'package:eyeoptic_app/services/auth.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class UserStore extends Auth {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  FirebaseAuth auth = FirebaseAuth.instance;
+
+  Future<void> createUserAccount(
+      UserModel model, Function()? onFinished) async {
+    try {
+      UserCredential userCredential = await auth.createUserWithEmailAndPassword(
+          email: model.emailAddress, password: model.password);
+
+      if (userCredential.user != null) {
+        await _firestore.collection('user').add({
+          'fname': model.fname,
+          'lname': model.lname,
+          'address': model.address,
+          'contactNo': model.contactNo,
+          'gender': model.gender,
+          'bday': model.bday,
+          'email': model.emailAddress,
+        }).then((value) {
+          if (onFinished != null) onFinished();
+        });
+      }
+    } catch (e) {
+      throw Exception(trimFirebaseMessage(e.toString()));
+    }
+  }
+}
